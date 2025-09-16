32d38f3697b7ee7cb1ecfe46cf3a762134a633c9
RizkySatria123 <rizky12satria@gmail.com>
2025-09-16 17:13:52 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,3 +11,10 @@ $routes->get('/dosen/display', 'Dosen::display');
 $routes->get('/mahasiswa/display','Mahasiswa::display');
 $routes->get('/berita', 'Berita::index');
 $routes->get('/data','Data::index');
+
+// Auth
+$routes->get('login', 'Auth::login');
+$routes->post('login', 'Auth::attempt');
+$routes->get('register', 'Auth::register');
+$routes->post('register', 'Auth::store');
+$routes->get('logout', 'Auth::logout');
